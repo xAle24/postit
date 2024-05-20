304610a7bd05cf993fd60a53b5221947107860c0
Alin Bordeianu <alinstefan.bordeianu@studio.unibo.it>
2024-05-20 23:35:54 +0200

    feat: added postit generation and deletion when page is resized; changed a random thing in css to make the scrollbars disappear

--- loginpage/login.js
+++ loginpage/login.js
@@ -1,4 +1,14 @@
-window.onload = function() {
+// This constant is used to avoid spawning the postits on the border of the screen
+const postitDiagonalDimension = 71
+
+window.onload = generatePostits
+window.addEventListener('resize', generatePostits)
+
+/**
+ * Generates 60 random postits on the screen.
+ */
+function generatePostits() {
+    clearPostits()
     var images = [
         '../resources/postit-no-writings/dark-green-logo-no-notice-board.png',
         '../resources/postit-no-writings/light-blue-logo-no-notice-board.png',
@@ -8,16 +18,24 @@ window.onload = function() {
         '../resources/postit-no-writings/purple-logo-no-notice-board.png',
         '../resources/postit-no-writings/red-logo-no-notice-board.png',
         '../resources/postit-no-writings/yellow-logo-no-notice-board.png',
-    ];
-    var background = document.getElementById('background');
-
-    for (var i = 0; i < 30; i++) {
-        var img = document.createElement('img');
-        img.src = images[Math.floor(Math.random() * images.length)];
+    ]
+    for (var i = 0; i < 60; i++) {
+        var img = document.createElement('img')
+        img.src = images[Math.floor(Math.random() * images.length)]
         img.className = 'randomImage';
-        img.style.top = Math.random() * window.innerHeight + 'px';
-        img.style.left = Math.random() * window.innerWidth + 'px';
-        img.style.transform = 'rotate(' + Math.random() * 360 + 'deg)';
-        document.body.appendChild(img);
+        img.style.top = Math.random() * (window.innerHeight - postitDiagonalDimension) + 'px'
+        img.style.left = Math.random() * (window.innerWidth - postitDiagonalDimension) + 'px'
+        img.style.transform = 'rotate(' + Math.random() * 360 + 'deg)'
+        document.body.appendChild(img)
     }
-};
+}
+
+/**
+ * Call this to clear the postits from the screen.
+ */
+function clearPostits() {
+    var postits = document.getElementsByClassName('randomImage')
+    while (postits.length > 0) {
+        postits[0].parentNode.removeChild(postits[0])
+    }
+}
