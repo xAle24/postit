2e63d5d240e38cf294050bbfb4505ef4a8ea7878
Alin Bordeianu <alinstefan.bordeianu@studio.unibo.it>
2024-05-30 17:37:34 +0200

    feat: added javascript code to create fieldsets for different availabilities in the post creation page

--- postcreationpage/postcreationpage.js
+++ postcreationpage/postcreationpage.js
@@ -3,13 +3,54 @@ let numberOfAvailabilities = 1
 document.getElementById('addAvailability').addEventListener('click', function() {
     var container = document.getElementById('availabilitiesList')
     var li = document.createElement('li')
-    var label = document.createElement('label')
-    label.textContent = 'Disponibilità ' + ++numberOfAvailabilities + ':'
-    var input = document.createElement('input')
-    input.type = 'datetime-local'
-    input.name = 'availabilities'
-    input.required = true
-    label.appendChild(input)
-    li.appendChild(label)
+    li.appendChild(createFieldset())
     container.appendChild(li)
 })
+
+/**
+ * Creates a fieldset with a legend and three inputs: date, start time and end time.
+ * @returns {HTMLFieldSetElement} the created fieldset
+ */
+function createFieldset() {
+    var fieldset = document.createElement('fieldset')
+    var legend = document.createElement('legend')
+    legend.textContent = 'Disponibilità ' + ++numberOfAvailabilities + ':'
+    fieldset.appendChild(legend)
+    fieldset.appendChild(createDateInputAndLabel())
+    fieldset.appendChild(createTimeInputAndLabel('dalle ore'))
+    fieldset.appendChild(createTimeInputAndLabel('alle ore'))
+    return fieldset
+}
+
+// TODO: understand how to create unique ids
+
+/**
+ * Creates a label with an input of type date.
+ * @returns {HTMLLabelElement} a label with an input of type date
+ */
+function createDateInputAndLabel() {
+    var label = document.createElement('label')
+    label.textContent = 'Data:'
+    var input = document.createElement('input')
+    input.type = 'date'
+    input.name = 'availabilityDate'
+    input.required = true
+    label.appendChild(input)
+    return label
+}
+
+/**
+ * Creates a label with an input of type time.
+ * @param {string} timeType a string representing the time type (e.g. 'dalle ore', 'alle ore') 
+ * @returns {HTMLLabelElement} a label with an input of type time
+ */
+function createTimeInputAndLabel(timeType) {
+    var label = document.createElement('label')
+    label.textContent = timeType
+    var input = document.createElement('input')
+    input.type = 'time'
+    input.name = 'startTime'
+    input.required = true
+    label.appendChild(input)
+    return label
+}
