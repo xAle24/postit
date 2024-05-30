9dd74551714a8d292fae994ce665c41f2a6292ee
Alin Bordeianu <alinstefan.bordeianu@studio.unibo.it>
2024-05-30 12:36:12 +0200

    chore: added gitattributes; saving before removing the insert post form

---
+++ homepage/homepage.js
@@ -0,0 +1,23 @@
+window.onload = onWindowLoad
+
+function onWindowLoad() {
+    console.log('Window loaded')
+    const posts = document.querySelectorAll('.post') // Get all the posts on the page
+    rotatePosts(posts) // Rotate the posts
+}
+
+function rotatePosts(posts) {
+    posts.forEach(post => {
+        const randomAngle = Math.random() * 6 - 3 // Generate a random angle between -3 and 3 degrees
+        console.log("Random angle: " + randomAngle)
+        post.style.transform = `rotate(${randomAngle}deg)` // Apply the rotation to the post
+    })
+}
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px"
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0"
+}
