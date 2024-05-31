211e9d20702443633f236a892165788dc940c563
Alin Bordeianu <alinstefan.bordeianu@studio.unibo.it>
2024-06-01 00:42:49 +0200

    feat: added most of the structure of the post detail page

--- postdetails/postdetails.js
+++ postdetails/postdetails.js
@@ -1,1 +1,11 @@
 // TODO: add function that creates the blurry layer in the background
+// TODO: create unique ids for each post
+
+let textarea = document.querySelector('.commentInputContainer textarea');
+textarea.addEventListener('input', autoResize, false);
+
+function autoResize() {
+    let linesCount = this.value.split('\n').length;
+    this.style.height = 'auto';
+    this.style.height = linesCount + 'lh'; // lh stands for line height
+}
