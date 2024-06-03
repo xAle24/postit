2cfff5c803743db40346bcb25546781435c5907d
Alin Bordeianu <alinstefan.bordeianu@studio.unibo.it>
2024-06-03 17:15:22 +0200

    feat: added php to upload files in a local directory; annoying redirection to php page

---
+++ educationalresourcespage/uploadresources.php
@@ -0,0 +1,30 @@
+<?php
+
+include "../db/db_connect.php";
+session_start();
+
+echo "<script>console.log(" . var_dump($_FILES) . ")</script>";
+
+$target_dir = "../database-content/uploads/"; // Specify the directory where file is going to be placed
+
+foreach ($_FILES as $key => $value) {
+    // $value["name"] is an array, so we need to iterate over it
+    for ($i = 0; $i < count($value["name"]); $i++) {
+        $target_file = $target_dir . basename($value["name"][$i]); // Specify the path of the file to be uploaded
+
+        // Check if file already exists
+        if (file_exists($target_file)) {
+            echo "Sorry, file " . $value["name"][$i] . " already exists.";
+            continue; // Skip to the next file
+        }
+
+        // Attempt to move the uploaded file to your desired location
+        if (move_uploaded_file($value["tmp_name"][$i], $target_file)) {
+            echo "The file ". $value["name"][$i]. " has been uploaded.";
+        } else {
+            echo "Sorry, there was an error uploading the file " . $value["name"][$i];
+        }
+    }
+}
+
+?>
