01c8a4209763e8cc2e9b597c47e1a76b96a0e62a
Alessio Lizza <alessio.lizza@studio.unibo.it>
2024-06-03 22:44:10 +0200

    changed to template method the creation of friend list

--- friendspage/friend.js
+++ friendspage/friend.js
@@ -1,32 +1,16 @@
 fetch('friend.php').then(response => response.json())
     .then(students => {
         students.forEach(student => {
-            // Create new div element
-            let div = document.createElement('div');
-            div.className = 'friendBlock';
-
-            let infoDiv = document.createElement('div');
-            infoDiv.className = 'friendInfo';
-            div.appendChild(infoDiv);
-            // Create img element
-            let img = document.createElement('img');
-            img.className = 'profileImg';
+            let template = document.createElement('template')
+            template.innerHTML = friendInfoTemplate.trim()
+            let img = template.content.querySelector('.profileImg')
             img.src = '../resources/straight-post-it.png';
-            infoDiv.appendChild(img);
-
-            // Create h2 element
-            let h2 = document.createElement('h2');
-            h2.className = 'profileName';
-            h2.textContent = student.name;  // Use the student's name from the PHP query result
-            infoDiv.appendChild(h2);
-
-            let followBtn = document.createElement('button');
-            followBtn.className = 'follow-button';
+            let h2 = template.content.querySelector('.profileName')
+            h2.textContent = student.name;
+            let followBtn = template.content.querySelector('.follow-button')
             followBtn.title = 'Follow';
             followBtn.textContent = 'Followed';
-            // Append the div to the body (or another container element)
-            div.appendChild(followBtn);
-            document.body.appendChild(div);
+            document.body.appendChild(template.content);
         });
     })
     .catch(error => console.error('Error:', error));
---
+++ friendspage/friendInfoTemplates.js
@@ -0,0 +1,12 @@
+const friendInfoTemplate = `
+<div class="friendBlock">
+    <div class="friendInfo">
+        <img class="profileImg">
+        </img>
+        <h2 class="profileName">
+        </h2>
+    </div>
+    <button class="follow-button">
+    </button>
+</div>
+`
