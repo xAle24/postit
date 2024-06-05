cb3e7adac6cd5adb8e2dbebf87ef850c92be4a70
Alin Bordeianu <alinstefan.bordeianu@studio.unibo.it>
2024-06-05 17:25:26 +0200

    boh commit a caso

--- db/queries.sql
+++ db/queries.sql
@@ -1,4 +1,5 @@
--- Used to see all the educational resources, their authors, filepath and the name of the subject they belong to
+-- USE THIS FILE AS A HELPER TO CREATE YOUR QUERIES IN PHP
+
 -- Used to see all the educational resources, their authors, filepath and the name of the subject they belong to
 SELECT `resourceName`, student.name, student.surname, `filePath`, subject.name as subject FROM `educational_resource`
 JOIN subject ON educational_resource.`subjectID` = subject.subjectID
