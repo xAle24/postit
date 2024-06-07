00f7065caf3b66237a4b5ca3f8805895e6eeb9a0
Alin Bordeianu <alinstefan.bordeianu@studio.unibo.it>
2024-06-07 18:58:36 +0200

    fix: handled error in fetchPosts function of homepage.js

--- achievementspage/loadAchievements.js
+++ achievementspage/loadAchievements.js
@@ -5,15 +5,17 @@ fetch('achievements.php').then(response => response.json())
             let template = document.createElement('template')
             template.innerHTML = achievementTemplate.trim()
             let img = template.content.querySelector('.achievementImg')
-            img.src = '../resources/straight-post-it.png';
+            img.alt = ''
             let h2 = template.content.querySelector('.achievementH2')
             h2.textContent = achievement.name;
             let p = template.content.querySelector('.achievementP')
             p.textContent = achievement.description;
             if (achievement.state === "1") {
+                img.src = '../resources/achievements/achievement-unlocked.svg';
                 document.body.insertBefore(template.content, document.querySelector(".lockedbanner"));
             } else {
                 document.body.appendChild(template.content);
+                img.src = '../resources/achievements/achievement-locked.svg';
             }
         });
     })
