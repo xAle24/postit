2fa73a9661c5b7f82ca3f1d67ab86877eda6dff3
Alin Bordeianu <alinstefan.bordeianu@studio.unibo.it>
2024-06-07 19:05:01 +0200

    removed groups

--- groupspage/groupspage.js
+++
@@ -1,9 +0,0 @@
-fetch('groupspage.php').then(response => response.json())
-.then(student => {
-    console.log("baobao" + JSON.stringify(student))
-        let img = document.getElementById("profile-img")
-        img.src = student.imagePath;
-        let groupsName = document.getElementById("groups-title")
-        groupsName.textContent = student.name;
-    })
-    .catch(error => console.error('Error:', error));
